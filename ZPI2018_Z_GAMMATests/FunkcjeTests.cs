ad0919556d984acf3186358549ba7694ce8f98ef
piotrektekkno <38582058+piotrektekkno@users.noreply.github.com>
2019-01-13 10:04:46 +0100

    TJ

---
+++ ZPI2018_Z_GAMMATests/FunkcjeTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPI2018_Z_GAMMATests
+{
+    class FunkcjeTests
+    {
+    }
+}
